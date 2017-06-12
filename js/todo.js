3ad01bf28b7ed3938ec626c31739c56b60285aa1
vidushimehta <vidushi.2010@gmail.com>
2017-06-12 14:56:10 +0530

    js added for delete list

--- js/todo.js
+++ js/todo.js
@@ -1,23 +1,34 @@
+
 function addValue() {
 
 	var inputValue = document.getElementById('data').value;
-	console.log(inputValue);
 	if(inputValue) {
-		var createli = document.createElement('li');
+		createli = document.createElement('li');
 		var createtext = document.createTextNode(inputValue);
 		createli.appendChild(createtext);
-		var button = document.createElement("button");
-		button.innerHTML = "Delete";
-		createli.appendChild(button);
-		console.log(button)
 
 		var divmain = document.getElementById('test');
 		divmain.appendChild(createli);
+
 	 	document.getElementById('data').value = '';
+	 
+	 	deleteValue(createli);
 	}
 	else
 	{
 		alert("please add a task")
-	}
+	}		
+	return false;
+}
+
+function deleteValue(createli) {
+
+	var deleteButton = document.createElement("button");
+	deleteButton.innerHTML = "Delete";
+	createli.appendChild(deleteButton);
+	deleteButton.setAttribute("id", "delete");
+	deleteButton.addEventListener("click", function() {	
+		createli.parentNode.removeChild(createli);		
+	})
 	return false;
 }
