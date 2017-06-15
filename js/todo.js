67b96624993deefec313ccc371bd01ba0836c7ae
vidushimehta <vidushi.2010@gmail.com>
2017-06-15 17:37:11 +0530

    js added for to-do-list

--- js/todo.js
+++ js/todo.js
@@ -1,26 +1,48 @@
-
+//Add value to the list
 function addValue() {
 
 	var inputValue = document.getElementById('data').value;
 	if(inputValue) {
-		createli = document.createElement('li');
+		var createli = document.createElement('LI');
+		createli.setAttribute('id', 'list')
+		var randomId = createli.children.length+1
+		var createSpan = document.createElement('span');
+		createSpan.setAttribute('id', randomId);
+		
+		createli.appendChild(createSpan);
 		var createtext = document.createTextNode(inputValue);
-		createli.appendChild(createtext);
+		// createli.appendChild(createtext);
 
+		createSpan.appendChild(createtext);
+		console.log(createli);
 		var divmain = document.getElementById('test');
 		divmain.appendChild(createli);
 
 	 	document.getElementById('data').value = '';
 	 
 	 	deleteValue(createli);
+	 	checkedClass();
+	 	editValue(createSpan, createli);
 	}
 	else
 	{
 		alert("please add a task")
 	}		
+
 	return false;
 }
 
+//Add checked class to li
+function checkedClass() {
+	var b = document.querySelector('ul');
+	b.addEventListener('click', function(ev) {
+	  if (ev.target.tagName === 'LI') {
+	    ev.target.classList.toggle('checked');
+	  }
+	}, false);
+}
+
+//Delete the list
 function deleteValue(createli) {
 
 	var deleteButton = document.createElement("button");
@@ -31,4 +53,29 @@ function deleteValue(createli) {
 		createli.parentNode.removeChild(createli);		
 	})
 	return false;
+}
+
+//Edit the list
+function editValue(createSpan, createli) {
+
+	var editButton =  document.createElement("button");
+	editButton.innerHTML = "Edit";
+	editButton.setAttribute = ('id', 'editbtn')
+	createli.appendChild(editButton);
+
+	editButton.addEventListener("click", function() {	
+		createSpan.setAttribute('contentEditable', 'true');
+		createSpan.focus();
+	})
+
+	// method for keypress 
+	createSpan.addEventListener('keypress', function(evt) {
+  	if (evt.which === 13) {
+      evt.preventDefault();
+    	createSpan.setAttribute('contentEditable', 'false');
+
+  	}
+  	return false;
+	});
+		
 }
